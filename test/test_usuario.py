eb54cf20f58deab63a45bcee39a0892419ac889a
CristopherSs <alanss2907@gmail.com>
2019-04-13 17:17:39 -0500

    test para cambio de nombre agregado

--- test/test_usuario.py
+++ test/test_usuario.py
@@ -13,6 +13,16 @@ class UsuarioTest(unittest.TestCase):
         expectativa = 'AlanSuarez'
         self.assertEqual(expectativa, actual)
 
+    def test_actualizar_nombre(self):
+        # Arrange
+        usuario = Usuario('Alan Cristopher', 'Suarez Suarez', 'alanss2907@gmail.com', 'GG:v')
+        usuario.actualizar_nombre('Cristopher', 'Suarez Suarez')
+        # Act
+        actual = usuario.obtener_apodo()
+        # Assert
+        expectativa = 'CristopherSuarez'
+        self.assertEqual(expectativa, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
